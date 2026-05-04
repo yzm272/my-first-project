146a7463f3e74b90246d55d08ce96d0f417685f7
yuzhenming <yuzhenmingixiang@163.com>
2026-05-04 18:16:23 +0800

    version 2.0.0 k1

--- test001/src/main/java/org/yuproject/Main.java
+++ test001/src/main/java/org/yuproject/Main.java
@@ -1,5 +1,7 @@
 package org.yuproject;
 
+import org.yuproject.controller.CelebrateCreateGithubRemoteController;
+
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
@@ -8,10 +10,13 @@ public class Main {
         // 查看 IntelliJ IDEA 建议如何修正。
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
-            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-            System.out.println("i = " + i);
-        }
+        CelebrateCreateGithubRemoteController celebrateCreateGithubRemoteController = new CelebrateCreateGithubRemoteController();
+        celebrateCreateGithubRemoteController.TestAccess();
+
+//        for (int i = 1; i <= 5; i++) {
+//            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
+//            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
+//            System.out.println("i = " + i);
+//        }
     }
 }
---
+++ test001/src/main/java/org/yuproject/controller/CelebrateCreateGithubRemoteController.java
@@ -0,0 +1,8 @@
+package org.yuproject.controller;
+
+public class CelebrateCreateGithubRemoteController {
+
+    public void TestAccess(){
+        System.out.println("-------------------China is good,local add file successfully--------we will check wether does it can push remote success-----------");
+    }
+}
