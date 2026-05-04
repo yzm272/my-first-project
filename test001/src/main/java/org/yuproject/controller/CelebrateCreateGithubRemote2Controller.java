f3bca1b9a7111304412355364596257542171109
yuzhenming <yuzhenmingixiang@163.com>
2026-05-04 18:54:17 +0800

    4

---
+++ test001/src/main/java/org/yuproject/controller/CelebrateCreateGithubRemote2Controller.java
@@ -0,0 +1,8 @@
+package org.yuproject.controller;
+
+public class CelebrateCreateGithubRemote2Controller {
+
+    public void TestAccess(){
+        System.out.println("-------------------China is good,local add file successfully--------we will check wether does it can push remote success-----------");
+    }
+}
